ffe8a366f2d983f2e11e8f048e7c784b963a6353
YKZIRPOLYGLOT <113853753+YKZIRPOLYGLOT@users.noreply.github.com>
2025-12-26 14:26:45 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const cacheName = 'turen-dashboard-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
